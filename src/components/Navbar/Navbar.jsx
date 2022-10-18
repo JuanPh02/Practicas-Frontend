a9e7579afca939eb5fa35fd25a1b522e4adfe8bc
JuanPh02 <juanpabloarroyave1@hotmail.com>
2022-10-18 18:30:43 -0500

    Correcciones Header y Pagina de Inicio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from "react-icons/fa";
 import "./Navbar.css";
-import udemLogo from "../../assets/images/udem_logo.png";
+import udemLogo from "../../assets/images/udem_logo2.png";
 import { Container } from 'reactstrap';
 import { Sidebar } from '../Sidebar/Sidebar';
 
@@ -18,7 +18,7 @@ const showSidebar = () => setSidebar(!sidebar);
                 <a href="/" className='navbar-logo'>
                     <img src={udemLogo} alt="UdeM-logo" />
                 </a>
-                <h3 className='title'>postúlate udem</h3>
+                <h3 className='title'>Postúlate UdeM</h3>
                 <FaIcons.FaListOl onClick={showSidebar} style={{ color: "#000" , fontSize: 34}} />
             </Container>
             <Sidebar sidebar={sidebar} showSidebar={showSidebar} />
