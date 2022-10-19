5b4ce63607f5254d774b4f9143a0388b535ffccf
Tiwar02 <50062640+Tiwar02@users.noreply.github.com>
2022-10-18 19:07:00 -0500

    Correciones Sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState} from 'react';
-import { Link } from 'react-router-dom';
 import * as FaIcons from "react-icons/fa";
 import "./Navbar.css";
 import udemLogo from "../../assets/images/udem_logo2.png";
@@ -13,13 +12,13 @@ const [sidebar, setSidebar] = useState(false);
 const showSidebar = () => setSidebar(!sidebar);
 
     return (
-        <div className='navbar'>
+        <div className= {sidebar ? "navbar slide" : "navbar"}>
             <Container>
                 <a href="/" className='navbar-logo'>
                     <img src={udemLogo} alt="UdeM-logo" />
                 </a>
                 <h3 className='title'>Postúlate UdeM</h3>
-                <FaIcons.FaListOl onClick={showSidebar} style={{ color: "#000" , fontSize: 34}} />
+                <FaIcons.FaBars onClick={showSidebar} style={{ color: "#FFF" , fontSize: 24}} />
             </Container>
             <Sidebar sidebar={sidebar} showSidebar={showSidebar} />
         </div>
