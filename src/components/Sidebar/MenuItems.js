670a179320704410d8b89b4bfc6bf9c636389a72
saratobonr <saratobon09@gmail.com>
2022-10-18 19:49:20 -0500

    Items del menú hamburguesa

---
+++ src/components/Sidebar/MenuItems.js
@@ -0,0 +1,30 @@
+
+const MenuItems = [
+    
+    {
+        title: "inicio",
+        path: "/",
+        icon: "fas fa-house",
+        cName: "nav-text", 
+    },
+    {
+        title: "Students",
+        path: "/students",
+        icon: "fas fa-users",
+        cName: "nav-text", 
+    },
+    {
+        title: "Empresas.",
+        path: "/companies",
+        icon: "fas fa-building",
+        cName: "nav-text", 
+    },
+    {
+        title: "Oferts",
+        path: "/oferts",
+        icon: "fas fa-address-card",
+        cName: "nav-text", 
+    }
+]
+
+export { MenuItems};
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./Sidebar.css";
+import { MenuItems } from './MenuItems';
 
 export const Sidebar= ({sidebar,showSidebar}) => {
 
@@ -10,6 +11,22 @@ export const Sidebar= ({sidebar,showSidebar}) => {
         <ul className='sidebar-items' onClick={showSidebar}>
           <li className='navbar-display'>
           </li>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <a href='/'>
+                  <i className={item.icon}></i>
+                  <span>{item.title}</span>
+                </a>
+              </li>
+            )
+          })}
+              <li className="nav-text" >
+                <a href="/" >
+                  <i className="fas fa-arrow-right-from-bracket"></i>
+                  <span>Salir</span>
+                </a>
+              </li>
         </ul>
       </nav>
     </>
